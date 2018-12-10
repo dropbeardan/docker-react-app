55e51249f5d87d050a74ff670a4778115aea5d46
dropbeardan <dan.ess.001@gmail.com>
2018-12-11 01:06:04 +1100

    Connect WebService2 to WebService1

--- WebService1/src/index.ts
+++ WebService1/src/index.ts
@@ -1,10 +1,28 @@
 import express from 'express';
+import axios from 'axios';
+
+import SERVICE2_URL from './SERVICE2_URL.json';
 
 const app = express();
 const port = 3001;
+const service2URL =
+	process.env.NODE_ENV === 'development'
+		? SERVICE2_URL.development
+		: SERVICE2_URL.production;
 
 app.get('/service1', (req: any, res: any) =>
 	res.send('Web Service 1 Response!')
+);
+
+app.get('/service2', (req: any, res: any) =>
+	axios
+		.get(`${service2URL}/service2`)
+		.then((response: any) => {
+			res.send(response.data);
+		})
+		.catch((error: any) => {
+			res.send(`ERROR requesting Web Service 2.\n ${error}`);
+		})
 );
 
 app.listen(port, () => console.log(`WebService1 listening on port ${port}!`));
