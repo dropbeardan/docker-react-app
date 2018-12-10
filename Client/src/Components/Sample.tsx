574bb97a26e2a1083b8811b84e52c1942efc6c5a
dropbeardan <dan.ess.001@gmail.com>
2018-12-11 01:54:50 +1100

    Connect Client to WebServices

--- Client/src/Components/Sample.tsx
+++ Client/src/Components/Sample.tsx
@@ -1,11 +1,57 @@
 import * as React from 'react';
 
-export class SampleComponent extends React.Component<{}, {}> {
+import axios from 'axios';
+
+import * as SERVICE1_URL from './SERVICE1_URL.json';
+
+const service1URL =
+	process.env.NODE_ENV === 'development'
+		? SERVICE1_URL.development
+		: SERVICE1_URL.production;
+
+interface InnerState {
+	service1Response: string;
+	service2Response: string;
+}
+
+export class SampleComponent extends React.Component<{}, InnerState> {
+	constructor(props: {}) {
+		super(props);
+
+		this.state = {
+			service1Response: `FETCHING ${service1URL}/service1`,
+			service2Response: `FETCHING ${service1URL}/service2`
+		};
+	}
+
+	componentDidMount() {
+		axios(`${service1URL}/service1`)
+			.then((response: any) =>
+				this.setState({ service1Response: response.data })
+			)
+			.catch((error: any) =>
+				this.setState({
+					service1Response: `ERROR FETCHING ${service1URL}/service1: ${error}`
+				})
+			);
+
+		axios(`${service1URL}/service2`)
+			.then((response: any) =>
+				this.setState({ service2Response: response.data })
+			)
+			.catch((error: any) =>
+				this.setState({
+					service2Response: `ERROR FETCHING ${service1URL}/service2: ${error}`
+				})
+			);
+	}
+
 	render() {
 		return (
 			<div
 				style={{
 					display: 'flex',
+					flexDirection: 'column',
 					justifyContent: 'center',
 					alignItems: 'center',
 					height: '100vh',
@@ -13,6 +59,12 @@ export class SampleComponent extends React.Component<{}, {}> {
 				}}
 			>
 				<h1>React App</h1>
+				<br />
+				<h2>SERVICE 1</h2>
+				<span>{this.state.service1Response}</span>
+				<br />
+				<h2>SERVICE 2</h2>
+				<span>{this.state.service2Response}</span>
 			</div>
 		);
 	}
